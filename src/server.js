96cad82bc132618c80775d6410225832b4b729f9
Wai Cheong Ng Kenny <kenny01123@Wais-MacBook-Pro.local>
2020-10-12 16:56:00 +0900

    Implemented get pokemon
    Co-authored-by: ninamatti <ninamatti@users.noreply.github.com>
    Co-authored-by: kenny01123 <kenny01123@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,9 +1,39 @@
 const pokeData = require("./data");
-
+const express = require("express");
 const setupServer = () => {
   /**
    * Create, set up and return your express server, split things into separate files if it becomes too long!
    */
+  const app = express();
+  // GET /api/pokemon
+
+  app.get("/api/pokemon/", (req, res) => {
+    res.send(pokeData.pokemon);
+  });
+
+  app.get("/api/pokemon/:n", (req, res) => {
+    console.log("req.params.n", req.params.n);
+    if (req.params.n !== undefined) {
+      const limit = req.params.n;
+      const result = pokeData.pokemon.slice(0, limit);
+      res.send(result);
+    }
+  });
+
+  // - `POST /api/pokemon`
+  // - It should add a Pokemon. For Basic Requirements, verification that the data is good is not necessary
+  app.post("/api/pokemon/:id/:name", (req, res) => {
+    const id = req.params.id;
+    const name = req.params.name;
+    pokeData.pokemon.push({ id, name }); //the array
+    res.sendStatus(201);
+  });
+
+  /* - `GET /api/pokemon`
+  - It should return the full list of Pokemon
+  - It is able to take a query parameter `limit=n` that makes the endpoint only return the first `n` Pokemon */
+
+  return app;
 };
 
 module.exports = { setupServer };
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -1,17 +1,39 @@
+const { setupServer } = require("../src/server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const { should } = require("chai");
+const pokeData = require("../src/data");
 chai.use(chaiHttp);
-const { setupServer } = require("../src/server");
+chai.should();
+const app = setupServer();
 
 /*
  * This sprint you will have to create all tests yourself, TDD style.
  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
  * The same kind of structure that you encountered in lecture.express will be provided here.
  */
-const server = setupServer();
 describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
-    request = chai.request(server);
+    request = chai.request(app);
+  });
+
+  describe("basic requirements", () => {
+    describe("GET /api/pokemon", () => {
+      it("should be able to get the whole list of pokemons", async () => {
+        const res = await request.get("/api/pokemon");
+        chai.expect(res.body.length).to.equal(151);
+      });
+      it("should only return the first n pokemon with limit query", async () => {
+        const res = await request.get("/api/pokemon/10");
+        chai.expect(res.body.length).to.equal(10);
+      });
+    });
+
+    /*     describe("POST /api/pokemon", () => {
+      it("should add a new pokemon", async () => {
+        const res = await request.post("/api/pokemon/152/Abraxus");
+      });
+    }); */
   });
 });
