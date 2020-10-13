80c9c5ae336466169345c2eb17c9eb4a5c19dd22
Wai Cheong Ng Kenny <kenny01123@Wais-MacBook-Pro.local>
2020-10-13 15:56:38 +0900

    final

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const pokeData = require("./data");
 const express = require("express");
 const setupServer = () => {
+  /**
+   * Create, set up and return your express server, split things into separate files if it becomes too long!
+   */
   const app = express();
 
   app.get("/api/pokemon/", (req, res) => {
